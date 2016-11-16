ea15085b5f34e1fb86d709f6d71015038357756a
desa0067 <dmaulin7@gmail.com>
2016-11-16 10:56:04 -0500

    Hello World Maulin

---
+++ src/HelloWorldMaulin.java
@@ -0,0 +1,8 @@
+public class HelloWorldMaulin {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World Maulin");
+    }
+
+}
