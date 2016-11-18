0901bb752b9b41512b0d01e5fb720b606a0cfa61
Mike Gordanier <gord0213@algonquinlive.com>
2016-11-17 21:37:40 -0500

    Created Ticket, RefunedTicket, BoughtTicket, Soloist, Conductor, Instrument

---
+++ src/BoughtTicket.java
@@ -0,0 +1,7 @@
+import java.util.Date;
+
+public class BoughtTicket extends Ticket{
+	public BoughtTicket(String id, Seat seat, Date date){
+		super(id, seat, date);
+	}
+}
---
+++ src/Soloist.java
@@ -0,0 +1,16 @@
+
+public class Soloist extends Musician{
+
+	private String instrumentExpertise;
+	
+	public  Soloist(Person per){
+		
+	}
+	public String getExpertise(){
+		return instrumentExpertise;
+	}
+	public void setExpertise(String expertise){
+		this.instrumentExpertise = expertise;
+	}
+	
+}
