3d4eeeceaf4f80b1b81ed9ee21e7c9c3ea3db2ad
Mike Gordanier <gord0213@algonquinlive.com>
2016-11-17 21:48:39 -0500

    Updated Instrument - Finished

--- src/Instrument.java
+++ src/Instrument.java
@@ -1,4 +1,12 @@
 
 public class Instrument {
 
+	private String name;
+	private boolean owned;
+	
+	public Instrument(String name, boolean owned){
+		this.name = name;
+		this.owned = owned;
+	}
+	
 }
