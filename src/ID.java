6da295706213838491237de4dd87ba79f67fcc91
James <lowe0084@algonquinlive.com>
2016-11-22 13:11:08 -0500

    Created address, musician, name, phonenumber, person, and personbulder classes

---
+++ src/ID.java
@@ -0,0 +1,15 @@
+
+public class ID {
+
+	private String id ;
+	private GetID getID;
+	ID()
+	{
+		id = getID.getIDString();
+	}
+	
+	public String getID()
+	{
+		return id;
+	}
+}
---
+++ src/PersonBuilder.java
@@ -0,0 +1,45 @@
+import java.util.Date;
+
+public class PersonBuilder extends Person{
+	
+	private Person person;
+	
+	public PersonBuilder(String gender, Date dob){
+		this.person = new Person();
+	}//end PersonBuilder constructor
+	
+	public PersonBuilder gender(String gender){
+		this.person.setGender(gender);
+		return this;
+	}//end gender method
+	
+	public PersonBuilder dob(Date dob){
+		this.person.setDOB(dob);
+		return this;
+	}//end dob method
+	
+	public PersonBuilder name(String name){
+		this.person.setName(name);
+		return this;
+	}//end name method
+	
+	public PersonBuilder age(int age){
+		//this.person.setAge(age);
+		return this;
+	}//end age method
+	
+	public PersonBuilder salutation(String salutation){
+		//this.person.setSalutation(salutation);
+		return this;
+	}//end salutation method
+	
+	public PersonBuilder address(String address){
+		this.person.setAddress(address);//still int i....
+		return this;
+	}//end address method
+	
+	public Person build(){
+		
+	}//end gender method
+	
+}//end PersonBuilder class
