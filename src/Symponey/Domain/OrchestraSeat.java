1f3273eb85c562ad664b217416748df807b0af0d
Maulin <dmaulin7@gmail.com>
2016-11-22 14:14:59 -0500

    All Seat Classes
    Inherited seat classes

---
+++ src/Symponey/Domain/OrchestraSeat.java
@@ -0,0 +1,12 @@
+package Symponey.Domain;
+
+public class OrchestraSeat extends Seat{
+
+	private String memberName;
+	
+	public OrchestraSeat(double price, String seatNumber) {
+		super(price, seatNumber);
+		this.memberName = memberName;
+	}
+	
+}
---
+++ src/Symponey/Domain/PitSeat.java
@@ -0,0 +1,12 @@
+package Symponey.Domain;
+
+public class PitSeat extends Seat{
+
+	private String memberName;
+	
+	public PitSeat(double price, String seatNumber) {
+		super(price, seatNumber);
+		this.memberName = memberName;
+	}
+	
+}
