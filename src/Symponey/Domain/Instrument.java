bdae86e0d84455a61c7f6d3c792a691748caf6dc
michael <michaelricher101@gmail.com>
2016-11-22 18:26:22 -0500

    updated

--- src/Symponey/Domain/Instrument.java
+++ src/Symponey/Domain/Instrument.java
@@ -1,17 +1,34 @@
 package Symponey.Domain;
-
+/**
+ * 
+ * @author Michael Gordanier
+ *
+ */
 public class Instrument {
 
 	private String name;
 	private boolean owned;
-	
+	/**
+	 * 
+	 * @param name The name of the instrument
+	 * @param owned Is this instrument owned
+	 */
 	public Instrument(String name, boolean owned){
 		this.name = name;
 		this.owned = owned;
 	}
+	/**
+	 * 
+	 * @return the Instrument name
+	 */
 	public String getInstrument(){
 		return name;
 	}
+	
+	/**
+	 * 
+	 * @return is the instrument owned
+	 */
 	public boolean getInstrumentOwned(){
 		return owned;
 	}
