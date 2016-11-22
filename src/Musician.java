b8e024b54c5a6f8311b0f4f83204bb75ee7e23c9
James <lowe0084@algonquinlive.com>
2016-11-22 16:56:56 -0500

    Updated Address, Musician, Name, PhoneNumber classes

--- src/Musician.java
+++ src/Musician.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
 
-public class Musician {
-
-}
+public class Musician{
+	
+	private Person person; 
+	private ArrayList<Instrument> instrument;
+	private String stageName; 
+	   
+	public Musician(Person per, Instrument inst, String StageName){ 
+		this.instrument.add(inst);
+	    this.person = per;
+	    this.stageName = StageName;
+	}//end Musician constructor 
+	   
+	public Instrument getInstrument(int i){ 
+		if(i < instrument.size())
+			return instrument.get(i);
+		else
+			return null;
+	} //end getInstrument method
+	   
+	public String getStageName(){ 
+		return stageName; 
+	} //end getStageName method
+	
+}//end Musician class
--- src/PhoneNumber.java
+++ src/PhoneNumber.java
@@ -1,4 +1,26 @@
 
-public class PhoneNumber {
+public class PhoneNumber { 
 
-}
+	private int nationCode; 
+	private int regionCode; 
+	private int areaCode; 
+	private int lastFour; 
+	private String name; 
+	   
+	public PhoneNumber(int nCode, int rCode, int aCode, int lFour, String name){ 
+	     this.nationCode = nCode;
+	     this.regionCode = rCode;
+	     this.areaCode = aCode;
+	     this.lastFour = lFour;
+	     this.name = name;
+	}//end PhoneNumber constructor 
+	   
+	public String getPhoneNumber(){ 
+			return "+"+ nationCode +"-("+ regionCode +")"+ areaCode+ "-"+ lastFour; 
+	}//end getPhoneNumber method 
+	   
+	public String getName(){ 
+		return name; 
+	}//end getName method 
+	   
+}//end PhoneNumber class
