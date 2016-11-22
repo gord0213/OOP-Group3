1edd9ab4744ef072ea735218d17e60a019e93306
Mike Gordanier <gord0213@algonquinlive.com>
2016-11-22 18:07:50 -0500

    Changed stuffffffff

--- src/Symponey/Domain/Musician.java
+++ src/Symponey/Domain/Musician.java
@@ -1,5 +1,27 @@
 package Symponey.Domain;
+import java.util.ArrayList;
 
-public class Musician {
-
-}
+public class Musician{
+	
+	private Person person; 
+	private ArrayList<Instrument> instrument;
+	private String stageName; 
+	   
+	public Musician(Person per, Instrument inst, String StageName){ 
+		this.instrument.add(inst);
+	    this.person = per;
+	    this.stageName = StageName;
+	}//end Musician constructor 
+	   
+	public Instrument getInstrument(int i){ 
+		if(i < instrument.size())
+			return instrument.get(i);
+		else
+			return null;
+	} //end getInstrument method
+	   
+	public String getStageName(){ 
+		return stageName; 
+	} //end getStageName method
+	
+}//end Musician class
