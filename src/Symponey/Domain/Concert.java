9b0e440e94cbf9ff4f3a1831b1722b6b9bf14917
Roseanne Page <page0118@algonquinlive.com>
2016-11-22 20:12:09 -0500

    concert tweaks and finished SheduledConcert class

--- src/Symponey/Domain/Concert.java
+++ src/Symponey/Domain/Concert.java
@@ -7,7 +7,7 @@ public class Concert extends ID{
 	private String name;
 	private ArrayList<Composition> composition;
 	private ArrayList<Musician> symphony;
-	private Venue venue;
+	
 	
 	public Concert(String name, Composition comp){
 		
@@ -16,14 +16,10 @@ public class Concert extends ID{
 		composition = new ArrayList<Composition>();
 		composition.add(comp);
 		symphony = new ArrayList<Musician>();
-		venue = null;
+		
 	}//end of constructor
 	
-	public Venue getVenue(){
 		
-		return venue;
-	}//end of get venue
-	
 	public String getName(){
 		
 		return name;
@@ -38,11 +34,6 @@ public class Concert extends ID{
 		
 		return symphony.get(i);
 	}//end of get musician
-	
-	public void setVenue(Venue venue){
-		
-		this.venue = venue;
-	}//end of set venue
 	
 	public void addComposition(Composition comp){
 		
@@ -63,6 +54,11 @@ public class Concert extends ID{
 		
 		symphony.remove(mus);
 	}//end of remove musician
+	
+	public Boolean isSame(Concert con){
+		
+		return this == con;
+	}//end of is same
 	
 	
 }//end of class
