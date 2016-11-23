5ee4d586268ac3388292a397ab75422b9fa578ba
Unknown <mira0047@algonquinlive.com>
2016-11-23 12:17:51 -0500

    testCast Done

--- src/Symponey/Test/Test_Transaction.java
+++ src/Symponey/Test/Test_Transaction.java
@@ -1,5 +1,6 @@
 package Symponey.Test;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import Symponey.Domain.Name;
 import Symponey.Domain.Seat;
@@ -14,7 +15,9 @@ public class Test_Transaction extends TestCase{
 	
 	private Transaction transaction;
 	private Ticket ticket;
-	
+	private Seat seat;
+	private Date date;
+	private Name name;
 	public Test_Transaction(String name) { super(name);	}
 
 	public static Test suite() { return new TestSuite(Test_Transaction.class);		}
@@ -28,20 +31,26 @@ public class Test_Transaction extends TestCase{
  	 */
 	public void testConstructors() {
 		System.out.println("\tExecuting Test_Transaction.testConstructors");
-		ticket = new Ticket()
-		transaction = new Transaction("online", "Miraz", 15.00, ticket);
-		assertNotNull("\t\tTest_ShannonsModel.testConstructors: model is null", ticket);
+		seat = new Seat(400.00,"A777");
+		date = new Date();
+		ticket = new Ticket(seat,date);
+		transaction = new Transaction("online", name, 15.00, ticket);
+		assertNotNull("\t\tTest_ShannonsModel.testConstructors: model is null", transaction);
 	}
 	
 	public void testAccessors(){
-		System.out.println("\t Executing Test_Ticket.testAccessors");
-		id = "Identification";
-		seat = new Seat(15, "D15");
+		System.out.println("\t Executing Test_Transaction.testAccessors");
+		seat = new Seat(400.00,"A777");
 		date = new Date();
-		ticket = new Ticket(id, seat, date);
-		assertNotNull("\t\tTest_Ticket.testAccessor: Ticket is null", ticket);
-		assertTrue("\t\tTest_Ticket.testAccessors.getID Failed", ticket.getID() == id);
-		assertTrue("\t\tTest_Ticket.testAccessors.getID Failed", ticket.getSeat() == seat);
-		assertTrue("\t\tTest_Ticket.testAccessors.getID Failed", ticket.getDate() == date);
+		ticket = new Ticket(seat,date);
+		transaction = new Transaction("online", name, 15.00, ticket);
+		String time = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
+
+		assertNotNull("\t\tTest_Transaction.testAccessor: Transaction is null", transaction);
+		assertTrue("\t\tTest_Transaction.testAccessors.getID Failed", transaction.getLedgerAccount() == "online");
+		assertTrue("\t\tTest_Transaction.testAccessors.getID Failed", transaction.getTime().substring(0, 10).equals("2016.11.23"));
+		assertTrue("\t\tTest_Transaction.testAccessors.getID Failed", transaction.getName() == name);
+		assertTrue("\t\tTest_Transaction.testAccessors.getID Failed", transaction.getTicket(5) == ticket);
+		assertTrue("\t\tTest_Transaction.testAccessors.getID Failed", transaction.getPrice() == 15.00);
 	}
 }
