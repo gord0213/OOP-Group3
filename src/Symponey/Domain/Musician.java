df0f412931650c85e84f63114c5522b3288e6914
Roseanne Page <page0118@algonquinlive.com>
2016-11-23 01:38:05 -0500

    cherrypick

--- src/Symponey/Domain/Musician.java
+++ src/Symponey/Domain/Musician.java
@@ -1,5 +1,33 @@
 package Symponey.Domain;
 
+<<<<<<< HEAD
 public class Musician {
 
 }
+=======
+public class Musician extends Person{
+	
+	private Person person; 
+	private ArrayList<Instrument> instrument;
+	private String stageName; 
+	   
+	public Musician(Person per, Instrument inst, String StageName){ 		
+		super(per);
+		this.instrument.add(inst);
+	    this.person = per;
+	    this.stageName = StageName;
+	}//end Musician constructor 
+	   
+	public Instrument getInstrument(int i){ 
+		if(i < instrument.size())
+			return instrument.get(i);
+		else
+			return null;
+	} //end getInstrument method
+	   
+	public String getStageName(){ 
+		return stageName; 
+	} //end getStageName method
+	
+}//end Musician class
+>>>>>>> 52b6776... More updated
