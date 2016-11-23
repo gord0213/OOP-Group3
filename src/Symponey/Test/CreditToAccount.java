932d7bbfc0f3f8ada8a0d79481aa24f99386c39f
Unknown <mira0047@algonquinlive.com>
2016-11-23 00:54:06 -0500

    Created

---
+++ src/Symponey/Test/CreditToAccount.java
@@ -0,0 +1,9 @@
+package Symponey.Test;
+
+public class CreditToAccount {
+
+	public CreditToAccount(String ledger, Name name, double price, Ticket ticket){
+		super(ledger, name, price,ticket);
+
+	}
+}
---
+++ src/Symponey/Test/Refund.java
@@ -0,0 +1,10 @@
+package Symponey.Test;
+
+public class Refund {
+	
+
+	public Refund(String ledger, Name name, double price, Ticket ticket){
+		super(ledger, name, price,ticket);
+
+}
+}
