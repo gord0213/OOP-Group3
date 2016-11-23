665783236d288d073240057b221e576b6b9a8bb2
Roseanne Page <page0118@algonquinlive.com>
2016-11-22 19:47:45 -0500

    finished concert and fixes to composition

--- src/Symponey/Domain/Composition.java
+++ src/Symponey/Domain/Composition.java
@@ -1,5 +1,70 @@
 package Symponey.Domain;
 
-public class Composition {
+import java.util.ArrayList;
 
-}
+public class Composition extends ID {
+
+	private ArrayList<Movement> move;
+	private Boolean approved;
+	private String composer;
+	private ArrayList<Soloist> soloist;
+	private Conductor conductor;
+	
+	public Composition(String composer){
+		
+		super();
+		move = new ArrayList<Movement>();
+		soloist = new ArrayList<Soloist>();
+		approved = false;
+		this.composer = composer;
+		conductor = null;
+	}//end of constructor
+	
+	public Soloist getSolo(int i){
+		
+		return soloist.get(i);
+	}//end of get soloist
+	
+	public Movement getMovement(int i){
+		
+		return move.get(i);
+	}//end of get movement
+	
+	public Boolean getApproved(){
+		
+		return approved;
+	}//end of get approved
+	
+	public String getComposer(){
+		
+		return composer;
+	}//end of get composer
+	
+	public Conductor getConductor(){
+		
+		return conductor;
+	}//end of get conductor
+	
+	public void setMovement(Movement move){
+		
+		this.move.add(move);
+	}//end of set movement(single)
+	
+	public void setMovement(Movement[] move){
+		
+		for(int i = 0; i < move.length; i++){
+			this.move.add(move[i]);
+		}//end of for loop
+	}//end of set movement(array)
+	
+	public void setConductor(Conductor con){
+		
+		conductor = con;
+	}//end of set conductor
+	
+	public void setApproved(Boolean approved){
+		
+		this.approved = approved;
+	}//end of setApproved
+	
+}//end of class
--- src/Symponey/Domain/Concert.java
+++ src/Symponey/Domain/Concert.java
@@ -1,5 +1,68 @@
 package Symponey.Domain;
 
-public class Concert {
+import java.util.ArrayList;
 
-}
+public class Concert extends ID{
+
+	private String name;
+	private ArrayList<Composition> composition;
+	private ArrayList<Musician> symphony;
+	private Venue venue;
+	
+	public Concert(String name, Composition comp){
+		
+		super();
+		this.name = name;
+		composition = new ArrayList<Composition>();
+		composition.add(comp);
+		symphony = new ArrayList<Musician>();
+		venue = null;
+	}//end of constructor
+	
+	public Venue getVenue(){
+		
+		return venue;
+	}//end of get venue
+	
+	public String getName(){
+		
+		return name;
+	}//end of get name
+	
+	public Composition getComposition(int i){
+		
+		return composition.get(i);
+	}//end of get composition
+	
+	public Musician getMusician(int i){
+		
+		return symphony.get(i);
+	}//end of get musician
+	
+	public void setVenue(Venue venue){
+		
+		this.venue = venue;
+	}//end of set venue
+	
+	public void addComposition(Composition comp){
+		
+		composition.add(comp);
+	}//end of add composition
+	
+	public void addMusician(Musician mus){
+		
+		symphony.add(mus);
+	}//end of add musician
+	
+	public void removeComposition(Composition comp){
+		
+		composition.remove(comp);
+	}//end of remove composition
+	
+	public void removeMusician(Musician mus){
+		
+		symphony.remove(mus);
+	}//end of remove musician
+	
+	
+}//end of class
