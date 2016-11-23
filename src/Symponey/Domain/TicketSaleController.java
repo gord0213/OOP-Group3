32445a51aa73564025c777b5bd1a916f3786d586
Mike Gordanier <gord0213@algonquinlive.com>
2016-11-23 15:19:59 -0500

    Got Proper TicketSaleController

--- src/Symponey/Domain/TicketSaleController.java
+++ src/Symponey/Domain/TicketSaleController.java
@@ -38,10 +38,6 @@ public class TicketSaleController {
 	{
 		this.con.get(this.con.size() - 1).addScheduledConcert(con);
 	}
-	public Concert getScheduledConcert(int i)
-	{
-		return con.get(con.size()-1).getScheduledConcert(i);
-	}
 	
 	//All of these are unimplemented process not required
 	public void addConcertSeason(ConcertSeason conSea)
