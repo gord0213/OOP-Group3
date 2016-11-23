87038206739c4e26cbcd5901116ab690a1a9a91f
Roseanne Page <page0118@algonquinlive.com>
2016-11-23 09:58:03 -0500

    test_venue wip

--- src/Symponey/Test/Test_Venue.java
+++ src/Symponey/Test/Test_Venue.java
@@ -1,5 +1,9 @@
 package Symponey.Test;
 
+import Symponey.Domain.Address;
+import Symponey.Domain.Composition;
+import Symponey.Domain.Instrument;
+import Symponey.Domain.Seat;
 import Symponey.Domain.Venue;
 import junit.framework.*;
 
@@ -10,7 +14,13 @@ import junit.framework.*;
  */
 public class Test_Venue extends TestCase{
 
-	private Venue venue;
+	private Venue venue, venue2;
+	private Address add = new Address("1", "1", "1", "1", "1", "1", "1", "blah");
+	private Address add2 = new Address("1", "1", "1", "1", "1", "1", "1", "second");
+	private Seat seat = new Seat(23, "G");
+	private Seat seat2 = new Seat(15, "F");
+	private Instrument instrument = new Instrument("flute", false);
+	private Instrument instrument2 = new Instrument("tuba", true);
 	
 	public Test_Venue(String name) { super(name);	}
 
@@ -19,5 +29,158 @@ public class Test_Venue extends TestCase{
 	protected void setUp() throws Exception { System.out.println("Test_Venue Begin");	}
 
 	protected void tearDown() throws Exception { System.out.println("Test_Venue End");	}
+	
+	/**
+ 	 * Test the constructors.
+ 	 */
+	public void testConstructors() {
+		System.out.println("\tExecuting Test_Venue.testConstructors");
+		
+		venue = new Venue(add, "name", seat);
+		assertNotNull("\t\tTest_Venue.testConstructors: Venue object mov is null", venue);
+	}//end of test constructor
+	
+	
+	/**
+ 	 * Test the getters.
+ 	 */
+	public void testGetters() {
+		System.out.println("\tExecuting Test_Venue.testGetters");
+		
+		//----------------------------------------testing getID----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add, "name2", seat);
+		
+		assertFalse("\t\tTest_Venue.testGetters.getID test 1 Failed", venue.getID() == venue2.getID());
+		
+		venue2 = venue;
+		assertTrue("\t\tTest_Venue.testGetters.getID test 2 Failed", venue.getID() == venue2.getID());
+		
+		//----------------------------------------testing getAddress----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+				
+		assertFalse("\t\tTest_Venue.testGetters.getAddress test 1 Failed", venue.getAddress() == venue2.getAddress());
+				
+		assertTrue("\t\tTest_Venue.testGetters.getAddress test 2 Failed", venue.getAddress() == add);
+		
+		//----------------------------------------testing getAddress----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+						
+		assertFalse("\t\tTest_Venue.testGetters.getName test 1 Failed", venue.getName() == venue2.getName());
+						
+		assertTrue("\t\tTest_Venue.testGetters.getName test 2 Failed", venue.getName() == "name");
+		
+		//----------------------------------------testing getSeat----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+		venue2.addSeat(seat);
+						
+		assertFalse("\t\tTest_Venue.testGetters.getSeat test 1 Failed", venue.getSeat(0) == venue2.getSeat(0));
+						
+		assertTrue("\t\tTest_Venue.testGetters.getSeat test 2 Failed", venue.getSeat(0) == venue2.getSeat(1));
+		
+		//----------------------------------------testing getInstrument----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+		venue.addInstrument(instrument);
+		venue.addInstrument(instrument2);
+		venue2.addInstrument(instrument2);
+								
+				assertFalse("\t\tTest_Venue.testGetters.getInstrument test 1 Failed", venue.getInstrument(0) == venue2.getInstrument(0));
+								
+				assertTrue("\t\tTest_Venue.testGetters.getInstrument test 2 Failed", venue.getInstrument(1) == venue2.getInstrument(0));
+		
+	}//end of test Getters
+	
+	/**
+ 	 * Test the setter.
+ 	 */
+	public void testSetter() {
+		System.out.println("\tExecuting Test_Venue.testSetter");
+		
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+						
+		assertFalse("\t\tTest_Venue.testSetter.setName test 1 Failed", venue.getName() == venue2.getName());
+						
+		venue2.setName("name");
+		assertTrue("\t\tTest_Venue.testSetter.setName test 2 Failed", venue.getName() == venue2.getName());
+		
+	}//end of test setter
+	
+	/**
+ 	 * Test the normal behaviors.
+ 	 */
+	public void testNormalBehaviors() {
+		System.out.println("\tExecuting Test_Venue.NormalBehaviors");
+		
+		//----------------------------------------testing addSeat----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+		venue2.addSeat(seat);
+		venue.addSeat(seat2);
+		
+		assertFalse("\t\tTest_Venue.testNormalBehaviors.addSeat test 1 Failed", venue.getSeat(0) == venue2.getSeat(0));
+		
+		assertTrue("\t\tTest_Venue.testNormalBehaviors.addSeat test 2 Failed", venue.getSeat(0) == venue2.getSeat(1));
+		
+		//----------------------------------------testing removeSeat----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+		venue2.addSeat(seat);
+		venue.addSeat(seat2);
+				
+		assertFalse("\t\tTest_Venue.testNormalBehaviors.removeSeat test 1 Failed", venue.getSeat(0) == venue2.getSeat(0));
+				
+		venue.removeSeat(seat);
+		assertTrue("\t\tTest_Venue.testNormalBehaviors.removeSeat test 2 Failed", venue.getSeat(0) == venue2.getSeat(0));
+		
+		//----------------------------------------testing addInstrument----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+		venue2.addInstrument(instrument2);
+		venue.addInstrument(instrument);
+				
+		assertFalse("\t\tTest_Venue.testNormalBehaviors.addInstrument test 1 Failed", venue.getInstrument(0) == venue2.getInstrument(0));
+		
+		venue2.addInstrument(instrument);
+		assertTrue("\t\tTest_Venue.testNormalBehaviors.addInstrument test 2 Failed", venue.getInstrument(0) == venue2.getInstrument(1));
+				
+		//----------------------------------------testing removeInstrument----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+		venue2.addInstrument(instrument2);
+		venue.addInstrument(instrument);
+		venue.addInstrument(instrument2);
+						
+		assertFalse("\t\tTest_Venue.testNormalBehaviors.removeInstrument test 1 Failed", venue.getInstrument(0) == venue2.getInstrument(0));
+						
+		venue.removeInstrument(instrument);
+		assertTrue("\t\tTest_Venue.testNormalBehaviors.removeInsturment test 2 Failed", venue.getInstrument(0) == venue2.getInstrument(0));
+		
+		//----------------------------------------testing checkInstrument----------------------------------------------
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+		venue2.addInstrument(instrument2);
+		venue.addInstrument(instrument);
+		
+								
+		assertFalse("\t\tTest_Venue.testNormalBehaviors.checkInstrument test 1 Failed", venue.checkInstrument(instrument2) == false);
+								
+		assertTrue("\t\tTest_Venue.testNormalBehaviors.checkInstrument test 2 Failed", venue.checkInstrument(instrument) == true);
+		
+		//----------------------------------------testing venueSoldOut----------------------------------------------
+		
+		venue = new Venue(add, "name", seat);
+		venue2 = new Venue(add2, "name2", seat2);
+		venue2.addSeat(seat);
+		venue.addSeat(seat2);
+				
+		assertFalse("\t\tTest_Venue.testNormalBehaviors.venueSoldOut test 1 Failed", venue.getSeat(0) == venue2.getSeat(0));
+				
+		assertTrue("\t\tTest_Venue.testNormalBehaviors.venueSoldOut test 2 Failed", venue.getSeat(0) == venue2.getSeat(1));
+	}//end of test Normal Behaviors
 	
 }//end of Test_Venue
