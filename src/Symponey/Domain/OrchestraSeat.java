971ad6c600d8514e434c38245fb65a5f3bd7d95a
Roseanne Page <page0118@algonquinlive.com>
2016-11-23 15:02:45 -0500

    cherrypick

--- src/Symponey/Domain/OrchestraSeat.java
+++ src/Symponey/Domain/OrchestraSeat.java
@@ -2,14 +2,16 @@ package Symponey.Domain;
 
 public class OrchestraSeat extends Seat{
 
+	private String memberName;
 	/**
 	 * Same as Seat this class is just for buisness processes.
 	 * @param price
 	 * @param seatNumber
 	 * @param memberName
 	 */	
-	public OrchestraSeat(double price, String seatNumber) {
+	public OrchestraSeat(double price, String seatNumber,String memberName) {
 		super(price, seatNumber);
+		this.memberName = memberName;
 	}
 	
 }
